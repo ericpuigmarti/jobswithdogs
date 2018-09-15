cd73793033cfd0c8d19e304317171a1be72ca24d
Eric Puigmarti <eric.puigmarti@gmail.com>
2018-09-15 13:06:38 -0400

    Site refresh ✨
    - fix font-syles
    - update illustration

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
-// 02 Search-Filters //
-//
-var $rows = $('#table tr');
-$('#search').keyup(function() {
-    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
-    
-    $rows.show().filter(function() {
-        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
-        return !~text.indexOf(val);
-    }).hide();
+// ------------------------ //
+// Search by - filter table
+// ------------------------ //
+
+var showResults;
+
+$('#search-box').keyup(function() {
+  var searchText;
+  searchText = $('#search-box').val();
+  return showResults(searchText);
+});
+
+showResults = function(searchText) {
+  $('tbody tr').hide();
+  return $('tbody tr:Contains(' + searchText + ')').show();
+};
+
+jQuery.expr[":"].Contains = jQuery.expr.createPseudo(function(arg) {
+  return function(elem) {
+    return jQuery(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+  };
 });
